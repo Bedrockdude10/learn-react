2554a4f399693ba07b1243d38df589b83f9fe615
bedrockdude10 <dannyrollo4@gmail.com>
2025-01-31 17:59:53 -0500

    fixed index out of bounds bug

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,13 +3,13 @@
 // import { Inter } from 'next/font/google'
 // import styles from '@/styles/Home.module.css'
 import Profile from './components/profile'
-import Gallery from './qcomps/gallery_props'
 import PackingList from './qcomps/props_item'
 import List from './components/list_plain'
 import RecipeList from './qcomps/recipes'
+import Gallery from './components/state'
 
 export default function Home() {
   return (
-    <RecipeList/>
+    <Gallery/>
   )
 }
